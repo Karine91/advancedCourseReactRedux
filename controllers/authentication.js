005118bdf51dcc213f32d6c38f7bd12fd5f998f2
Karine91 <kitana.91@mail.ru>
2018-11-07 17:13:49 +0200

    init commit

---
+++ controllers/authentication.js
@@ -0,0 +1,42 @@
+const User = require("../models/user");
+const jwt = require("jwt-simple");
+const config = require("../config");
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signup = function(req, res, next) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ error: "You must provide email and password" });
+  }
+
+  User.findOne({ email }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (user) {
+      return res.status(422).send({ error: "Email is in use" });
+    }
+
+    const newUser = new User({
+      email,
+      password
+    });
+
+    newUser.save(function(err) {
+      if (err) {
+        return next(err);
+      }
+
+      res.json({ token: tokenForUser(newUser) });
+    });
+  });
+};
